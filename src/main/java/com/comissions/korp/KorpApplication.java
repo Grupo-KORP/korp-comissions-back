3a0564b918231d9e72e55bd3f6ed5c0623c65594
Matheus <matheus.csoares@sptech.school>
2026-02-27 19:20:28 -0300

    feat: commit inicial com o projeto spring com drivers spring web e mysql driver

---
+++ src/main/java/com/comissions/korp/KorpApplication.java
@@ -0,0 +1,13 @@
+package com.comissions.korp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comissions/korp/KorpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comissions.korp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
